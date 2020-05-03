e6e7c53ebcaddc67cb69cdb6d05f859e59e9de2d
Donald Barbosa <ecomponent.dbarbosa@voegol.com.br>
2020-05-03 02:35:57 -0300

    Finalização do método de cadastro da instituição e integração do método com a tela (Não foi terminada)
    --Ajuste na seleção do UF e Type. Antes estava hard coded

--- src/Habbib/Main.java
+++ src/Habbib/Main.java
@@ -1,17 +1,66 @@
 package Habbib;
 
-import Habbib.view.View;
+import Habbib.controller.RequisitionController;
+import Habbib.dao.BedDAO;
+import Habbib.dao.InstitutionDAO;
+import Habbib.dao.PatientDAO;
+import Habbib.model.Bed;
+import Habbib.model.Institution;
+import Habbib.model.Patient;
+import Habbib.model.Requisition;
+
+import java.util.ArrayList;
+
+import java.sql.Date;
 
 public class Main {
 
-    public static void main(String[] args) {
-        try
-        {
-            View view = new View();
+    public static <DATE> void main(String[] args) throws Exception {
+
+        RequisitionController require = new RequisitionController();
+        InstitutionDAO institutionDAO = new InstitutionDAO();
+        Institution institution = new Institution();
+        Institution institution2 = new Institution();
+        ArrayList<Requisition> requisitions;
+        Patient patient = new Patient();
+        PatientDAO patientDAO = new PatientDAO();
+        Bed bed = new Bed();
+        BedDAO bedDAO = new BedDAO();
+
+        try {
+            institution = institutionDAO.getInstitutionByName("teste");
+            institution2 = institutionDAO.getInstitutionByName("Hospital1");
+            requisitions = require.listRequisitions(institution);
+
+            System.out.println("Retorno");
+
         }
-        catch (Exception ex)
-        {
-            System.out.println(ex.getMessage());
+        catch (Exception e){
+            throw e;
         }
+
+        int year = 1999-1900;
+        Date date = new Date( year, 11, 12);
+
+
+        patient.setCid("A-50");
+        patient.setGender("Feminino");
+        patient.setCpf("3333");
+        patient.setDob(date);
+        patient.setFirstName("Sona");
+        patient.setLastName("Sustenido");
+        ArrayList<Bed> beds = bedDAO.getBedByInstitution(institution2);
+        bed.setInstitution(institution2);
+        bed.setType("UTI");
+
+        patientDAO.addPatient(patient);
+        require.createRequisition(patient, beds.get(0), institution, "Super teste");
+        Requisition requisition2 = new Requisition();
+        requisition2.setId(requisitions.get(0).getId());
+        requisition2.setStatus("Aprovado");
+        require.updateRequisitionStatus(requisition2);
+
+        System.out.println(patient.getDob());
+
     }
 }
--- src/Habbib/view/View.java
+++ src/Habbib/view/View.java
@@ -221,8 +221,6 @@ public class View extends BaseView{
 
         setVisible(true);
         return registerContainer;
-
-
     }
 
     private Container initMenu(Institution institution){
